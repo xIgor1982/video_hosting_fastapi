5d1ae8401513f6d4c93adf9872a2f9328c393eed
Igor <khrustch.igor@vniizht.ru>
2022-09-05 15:48:28 +0300

    #3 время: 0.00

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import shutil
-from fastapi import APIRouter, UploadFile, File, Form
+from typing import List
+from fastapi import APIRouter, UploadFile, File, Form, Request
 from fastapi.responses import JSONResponse
 from schemas import UploadVideo, User, GetVideo, Message
 
@@ -12,10 +13,28 @@ async def root(title: str = Form(...), description: str = Form(...), file: Uploa
 
 	return {"file_name" : file.filename}
 
+
+@video_router.post("/img", status_code=201)
+async def upload_image(files: List[UploadFile] = File(...)):
+	for img in files:
+		with open(img.filename, "wb") as buffer:
+			shutil.copyfileobj(img.file, buffer)
+	
+	return {"file_name" : "Good"}
+
+
 @video_router.get("/video", response_model=GetVideo, responses={404: {"model": Message}})
 async def get_video():
 	user = User(**{'id': 25, 'name': 'Ivan'})
 	video = UploadVideo(title='Test', description='Description')
 	all_data = {'user':user, 'video':video}
+	info = GetVideo(**all_data)
 	# return GetVideo(**all_data)
-	return JSONResponse(status_code=404, content={'message':'Not found'})
+	# return JSONResponse(status_code=404, content={'message':'Not found'})
+	return JSONResponse(status_code=200, content=info.dict())
+
+
+@video_router.get("/test")
+async def get_test(req: Request):
+	print(req.base_url)
+	return {}
